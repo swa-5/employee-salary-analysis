b1ba149dae959931dba258c7847b5c643bdd46c5
Swatantra Pandey <157131963+swa-5@users.noreply.github.com>
2026-02-28 01:31:03 +0530

    Create database and employees table with data

---
+++ sql/database_setup.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE company_db;
+USE company_db;
+
+CREATE TABLE employees (
+    emp_id INT PRIMARY KEY,
+    name VARCHAR(50),
+    department VARCHAR(50),
+    salary INT,
+    performance_rating INT
+);
+
+INSERT INTO employees VALUES
+(1, 'Aman', 'IT', 60000, 4),
+(2, 'Riya', 'HR', 45000, 3),
+(3, 'Rahul', 'IT', 75000, 5),
+(4, 'Neha', 'Finance', 50000, 4),
+(5, 'Arjun', 'IT', 72000, 5),
+(6, 'Simran', 'HR', 48000, 4),
+(7, 'Vikas', 'Finance', 52000, 3),
+(8, 'Pooja', 'IT', 68000, 4);
